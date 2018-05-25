cf51023608c3ce9bf409a70041828fa86b61f0e3
gosset <bgt-1999@hotmail.fr>
2018-05-25 22:43:03 +0200

    Amélioration de inscrire.php

---
+++ aja.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+
+	jQuery("#more_com").click(function({
+
+		$.ajax();
+
+	}));
+	
+});
--- inscrire.php
+++ inscrire.php
@@ -8,10 +8,31 @@
 
 	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
 
-	$stmt = $bdd->prepare(<<<SQL
+	$stmt=$bdd->prepare(<<<SQL
+        SELECT email, mot_de_passe
+        FROM membre 
+        WHERE email = :email
+        OR mot_de_passe = :mot_de_passe;
+SQL
+  ) ;
+        $stmt->execute(array(":email" => $_POST['email'], ":mot_de_passe" => $_POST['mot_de_passe']));
+        $exist=$stmt->fetch();
+
+        if($exist != null)
+        {
+        	header('Location: inscription.html');
+        }
+
+        else
+        {
+        	$stmt = $bdd->prepare(<<<SQL
           INSERT INTO MEMBRE (nom, prenom, pseudo, email, age, mot_de_passe)
           VALUES(?, ?, ?, ?, ?, ?);
 SQL
       ) ;
     $stmt->execute(array($_POST['nom'], $_POST['prenom'], $_POST['pseudo'], $_POST['email'], $_POST['age'], $_POST['mot_de_passe']));
+
+        }
+
+	
 	echo $p->toHTML();
